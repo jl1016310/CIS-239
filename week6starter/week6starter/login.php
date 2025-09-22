a7d57f956390e50648e731e7f9652e65d5a03f88
jl1016310 <jl1016310@otc.edu>
2025-09-22 10:02:01 -0500

    Add files via upload

---
+++ week6starter/week6starter/login.php
@@ -0,0 +1,27 @@
+<?php 
+    $login = $_GET['login'] ?? null;
+    if ($login === 'false')
+    {
+        header('Location: login.php');
+        exit;
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <?php require __DIR__ . '/includes/bootstrapcdnlinks.php'; ?>
+</head>
+
+<body class="p-3">
+    <?php require __DIR__ . '/includes/navigation.php'; ?>
+    <div class="container">
+        <h1>Login</h1>
+        <p><a class="btn btn-outline-primary" href="subscribe.php?login=yes">Send sample data via GET</a></p>
+    </div>
+</body>
+
+</html>
---
+++ week6starter/week6starter/subscribe.php
@@ -0,0 +1,28 @@
+<?php 
+    $login = $_GET['login'] ?? null;
+    if ($login === 'false')
+    {
+        header('Location: login.php');
+        exit;
+    }
+    ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Subscribe</title>
+    <?php require __DIR__ . '/includes/bootstrapcdnlinks.php'; ?>
+</head>
+
+<body class="p-3">
+    <?php require __DIR__ . '/includes/navigation.php'; ?>
+    <div class="container">
+        <h1>Subscribe</h1>
+
+        
+        
+    </div>
+</body>
+
+</html>
